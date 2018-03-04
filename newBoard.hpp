a65f9453b591217fd576a2f3419c07fa8361445a
Alessio-Amaolo <alessioamaolo@gmail.com>
2018-03-04 12:21:47 +0000

    Added new board files for binary representation.

---
+++ newBoard.hpp
@@ -0,0 +1,35 @@
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#include <bitset>
+#include "common.hpp"
+using namespace std;
+
+class Board {
+
+private:
+    bitset<64> black;
+    bitset<64> white;
+
+    bool occupied(int x, int y);
+    bool get(Side side, int x, int y);
+    void set(Side side, int x, int y);
+    bool onBoard(int x, int y);
+
+public:
+    Board();
+    ~Board();
+    Board *copy();
+
+    bool isDone();
+    bool hasMoves(Side side);
+    bool checkMove(Move *m, Side side);
+    void doMove(Move *m, Side side);
+    int count(Side side);
+    int countBlack();
+    int countWhite();
+
+    void setBoard(char data[]);
+};
+
+#endif
