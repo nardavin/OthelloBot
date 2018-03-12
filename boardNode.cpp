eb682863cfd3ae36629a7f401e634e5a58026489
Nicholas Ardavin <nardavin@github.com>
2018-03-11 20:38:15 -0700

    Implemented a 20-move end game solver. No time control yet, so this may completely fail

--- boardNode.cpp
+++ boardNode.cpp
@@ -39,8 +39,15 @@ BoardNode::~BoardNode(){
  * Gets the move that was used to get from the previous board to this one
  * @return Move that was used to get from previous board to this one
  */
-Move BoardNode::getMove(){
-    return *move;
+Move* BoardNode::getMove(){
+    if(move == nullptr){
+        return nullptr;
+    }
+    return new Move(move->getX(), move->getY());
+}
+
+vector<BoardNode*> BoardNode::getChildren(){
+    return children;
 }
 
 /**
@@ -64,14 +71,15 @@ float BoardNode::searchTreeAB(int depth, float alpha, float beta,
     }
     for(int i = 0; i < (int)possibleMoves.size(); i++){
         children.push_back(new BoardNode(board, possibleMoves[i], movingSide));
-    }
-    for(int i = 0; i < (int)children.size(); i++){
         float score = -children[i]->searchTreeAB(depth - 1, -beta, -alpha, heuristic);
         alpha = max(alpha, score);
         if(alpha >= beta) break;
     }
     for(int i = 0; i < (int)children.size(); i++){
         delete children[i];
+    }
+    for(int i = (int)children.size(); i < (int)(int)possibleMoves.size(); i++){
+        delete possibleMoves[i];
     }
     children.clear();
     return alpha;
@@ -101,8 +109,6 @@ float BoardNode::searchTreePVS(int depth, float alpha, float beta,
     }
     for(int i = 0; i < (int)possibleMoves.size(); i++){
         children.push_back(new BoardNode(board, possibleMoves[i], movingSide));
-    }
-    for(int i = 0; i < (int)children.size(); i++){
         float score;
         if(i == 0){
             score = -children[i]->searchTreePVS(depth - 1, -beta, -alpha, heuristic);
@@ -119,20 +125,63 @@ float BoardNode::searchTreePVS(int depth, float alpha, float beta,
     for(int i = 0; i < (int)children.size(); i++){
         delete children[i];
     }
+    for(int i = (int)children.size(); i < (int)(int)possibleMoves.size(); i++){
+        delete possibleMoves[i];
+    }
     children.clear();
     return alpha;
 }
 
-/*
-float BoardNode::searchTreeEndGame(float (*heuristic)(Board*, bool)), bool ourSide){
+
+float BoardNode::searchTreeEndGame(float (*heuristic)(Board*, bool), bool ourSide){
     bool movingSide = !side;
     if(board->countMoves(!movingSide) == 0 && board->countMoves(movingSide) == 0){
-
+        //cerr << "hi" << endl;
+        return (*heuristic)(board, ourSide);
     }
     vector<Move*> possibleMoves = board->possibleMoves(movingSide);
+    if(possibleMoves.size() == 0){
+        possibleMoves.push_back(nullptr);
+    }
+    if(movingSide == ourSide){
+        for(int i = 0; i < (int)possibleMoves.size(); i++){
+            BoardNode *testNode = new BoardNode(board, possibleMoves[i], movingSide);
+            float score = testNode->searchTreeEndGame(heuristic, ourSide);
+            if(score == 1){
+                children.push_back(testNode);
+                for(int j = i + 1; j < (int)possibleMoves.size(); j++){
+                    delete possibleMoves[j];
+                }
+                return 1;
+            }
+            else{
+                delete testNode;
+            }
+        }
+        return -1;
+    }
+    else{
+        for(int i = 0; i < (int)possibleMoves.size(); i++){
+            BoardNode *testNode = new BoardNode(board, possibleMoves[i], movingSide);
+            float score = testNode->searchTreeEndGame(heuristic, ourSide);
+            children.push_back(testNode);
+            if(score < 1){
+                for(int j = 0; j <= i; j++){
+                    delete children[j];
+                }
+                for(int j = i + 1; j < (int)possibleMoves.size(); j++){
+                    delete possibleMoves[j];
+                }
+                children.clear();
+                return -1;
+            }
+        }
+        return 1;
+    }
 
 }
-*/
+
+
 /**
  * Finds the best move to make this round using minimax and A/B pruning
  * @param  depth     Depth to search in the node tree
@@ -150,11 +199,12 @@ Move* BoardNode::getBestChoice(int depth, float (*heuristic)(Board*, bool), bool
     }
     float alpha = -numeric_limits<float>::max();
     float beta = numeric_limits<float>::max();
-    Move ret = Move(0, 0);
+    Move* ret = new Move(0, 0);
     for(int i = 0; i < (int)children.size(); i++){
         float score = -children[i]->searchTreePVS(depth - 1, -beta, -alpha, heuristic);
         if(score > alpha){
             alpha = score;
+            delete ret;
             ret = children[i]->getMove();
         }
     }
@@ -162,7 +212,7 @@ Move* BoardNode::getBestChoice(int depth, float (*heuristic)(Board*, bool), bool
         delete children[i];
     }
     children.clear();
-    return new Move(ret.getX(), ret.getY());
+    return ret;
 }
 
 /**
